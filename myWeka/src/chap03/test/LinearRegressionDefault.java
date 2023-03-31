4424f6e2e1fed91e390594b188ac6b1bd100e0c7
DongHyun <ehdgus6709@naver.com>
2023-03-31 11:46:36 +0900

    study

--- myWeka/src/chap03/test/LinearRegressionDefault.java
+++ myWeka/src/chap03/test/LinearRegressionDefault.java
@@ -8,7 +8,7 @@ public class LinearRegressionDefault {
 		LinearRegression model = new LinearRegression();
 		String fileName = "regression_outliers";
 		ModelApply modelApply = new ModelApply(fileName, model);
-		
+	
 		modelApply.plot2DInstances("outlier recognition", 0, 1);
 		
 		System.out.println("[ 모델 정보 & 평가 결과 ]");
